19d7ed94b46d2fc435ee167ed56f649457c4805c
itlackey <itlackey@gmail.com>
2024-11-18 00:37:03 -0600

    v0.0.6 - load .env in current dir when CLI is called

--- src/cli.js
+++ src/cli.js
@@ -4,10 +4,16 @@ import { readFileSync } from "fs";
 import { Command } from "commander";
 import { generate } from "./generate.js";
 import { init } from "./init.js";
+import dotenv from "dotenv";
 
 const packageJson = JSON.parse(
   readFileSync(new URL("../package.json", import.meta.url), "utf-8")
 );
+
+// load .env file
+dotenv.config({ override: true });
+
+// Create a new Command object and set its name and description.
 
 const program = new Command();
 
@@ -37,35 +43,38 @@ program
     try {
       await generate(cssPattern, output, promptFile);
     } catch (err) {
-      console.error("Cassi: I encountered an error while generating markdown:", err.message);
+      console.error(
+        "Cassi: I encountered an error while generating markdown:",
+        err.message
+      );
       process.exit(1);
     }
   });
 
-  //Subcommand: init
-  program
-    .command("init")
-    .description(
-      "Initialize a new Eleventy project with the generated markdown files"
-    )
-    .argument("<outputDir>",
-      "Directory to save generated markdown files"
-    )
-    .option(
-      "--tag <version>",
-      "Version of the starter kit to use. Defaults to 'latest'",
-      "v0.0.1"
-    )
-    .action(async (outputDir, options) => {
-      const { tag } = options;
+//Subcommand: init
+program
+  .command("init")
+  .description(
+    "Initialize a new Eleventy project with the generated markdown files"
+  )
+  .argument("<outputDir>", "Directory to save generated markdown files")
+  .option(
+    "--tag <version>",
+    "Version of the starter kit to use. Defaults to 'latest'",
+    "v0.0.1"
+  )
+  .action(async (outputDir, options) => {
+    const { tag } = options;
 
-      try {
-        await init(outputDir, tag);
-      } catch (err) {
-        console.error("Cassi: I encountered an error while initializing Eleventy project:", err.message);
-        process.exit(1);
-      }
-    });
-
+    try {
+      await init(outputDir, tag);
+    } catch (err) {
+      console.error(
+        "Cassi: I encountered an error while initializing Eleventy project:",
+        err.message
+      );
+      process.exit(1);
+    }
+  });
 
 program.parse(process.argv);
